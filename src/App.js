7f7f4de67045720d5a0a79fe8b7e16cf13a410d5
Paulo Faria <santos.paulo.rf@gmail.com>
2022-08-26 03:19:12 -0300

    :sparkles: Choosing api. Setting up connection with fetch

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
+import React, { useState } from "react"
 
 
 function App() {
+
+  const [dataAnimals, setDataAnimals] = useState({})
+
+
+  const getDataApi = async () => {
+    fetch("https://zoo-animal-api.herokuapp.com/animals/rand")
+    .then((res) => res.json())
+    .then((data) => {
+      setDataAnimals(data)
+      console.log(data)
+
+    })
+  }
+
+
   return (
-    <div className="">
-      <h1> Hello </h1>
+    <div 
+      className="flex justify-center p-10"
+    >
+      <button
+        className="border-2 bg-blue-700 text-white hover:bg-blue-300 p-3 transition-all ease-in-out duration-300 shadow-md rounded-lg"
+        onClick={() => getDataApi()}
+      >
+        Push me
+      </button>
     </div>
   );
 }
